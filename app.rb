b2a2dd69900f0d42328418a570dbac98470d0516
Amina Jusic <amina_jusic@yahoo.com>
2019-02-19 17:01:26 +0000

    Entering players

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
     get'/' do
-      "Testing infrastructure working!"
+      erb :index
     end
+
+post '/names' do
+  @player_1_name = params[:player_1_name]
+  @player_2_name = params[:player_2_name]
+  "Player 1: #{@player_1_name} Player 2: #{@player_2_name} "
+end
+
 
 run! if app_file == $0
 
--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,9 +1,9 @@
 feature "Player names" do
   scenario "Players fill in forms and submit them and see the names on screen" do
     visit('/')
-    fill_in("player 1 name", with: "Steve")
-    fill_in("player 2 name", with: "Amina")
+    fill_in("player_1_name", with: "Steve")
+    fill_in("player_2_name", with: "Amina")
     click_button("Submit")
-    expect(page).to have_content "Player 1: Steve\n Player 2: Amina"
+    expect(page).to have_content "Player 1: Steve Player 2: Amina"
   end
 end
