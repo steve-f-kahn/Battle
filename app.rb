43aaa9712773e1f0237728f2c2b1d3fb08fb596a
Steve Kahn <steve@kahn.org.uk>
2019-02-22 17:26:20 +0000

    Fixed code after removal of global variables

--- app.rb
+++ app.rb
@@ -13,26 +13,26 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(name: params[:player_1_name], hp: 100)
     player_2 = Player.new(name: params[:player_2_name], hp: 100)
-    game = Game.new(player_1,player_2)
+    Game.start_game(player_1,player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = game.player_1.name
-    @player_2_name = game.player_2.name
-    @player_2_hp = game.player_2.hp
+    @player_1_name = Game.current_game.player_1.name
+    @player_2_name = Game.current_game.player_2.name
+    @player_2_hp = Game.current_game.player_2.hp
     erb :play
   end
 
   post '/player-1-attack' do
-    game.attack(game.player_2, 10)
-    game.player_turn_change
+    Game.current_game.attack(Game.current_game.player_2, 10)
+    Game.current_game.player_turn_change
     redirect '/play'
   end
 
   post '/player-2-attack' do
-    game.attack(game.player_1, 10)
-    game.player_turn_change
+    Game.current_game.attack(Game.current_game.player_1, 10)
+    Game.current_game.player_turn_change
     redirect '/play'
   end
 
--- models/game.rb
+++ models/game.rb
@@ -1,4 +1,13 @@
 class Game
+
+  def self.start_game(player_1, player_2)
+    @current_game = Game.new(player_1, player_2)
+  end
+
+  def self.current_game
+    @current_game
+  end
+
   attr_reader :player_1, :player_2, :player_turn
   def initialize(player_1, player_2)
     @player_1 = player_1
